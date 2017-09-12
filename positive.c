8bc3627d1eeb6586287d3dc8a5d6dadb214e13d9
anithaarun <31502990+anithaarun@users.noreply.github.com>
2017-09-12 15:25:14 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+a=2;
+b=-4;
+c=8;
+{
+if(a%2==0)
+{
+printf("a is positive");
+scanf("%d",&a);
+}
+else if
+{
+printf("b is positive");
+}
+else if
+{
+printf("c is positive")
+}
+else
+{
+printf("all are negative");
+}
+}
+}

72dffa2489b3ed17b401b2b11c977eae8b73a0d5
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-18 16:33:20 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+a=20;
+b=40;
+c=25;
+if(20<40)
+{
+printf("b is largest");
+scanf("%d",&b);
+}
+elseif(25>20)
+{
+printf("c is largest");
+scanf("%d",&c);
+}
+else
+{
+printf("a is largest");
+scanf("%d",&a);
+}
+}

b3772c2fb4dbf21b4776ca905e72c895ac0b9cf6
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-18 16:26:42 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int alphabets;
+printf("enter the alphabets");
+scanf("%d",&alphabets);
+if(alphabet=A,B,C)
+{
+printf("the given letter is alphabet");
+}
+else
+{
+printf("not alphabet")
+}
+return 0;
+}

e130ec91d662e7821cd518f978fa32bb4f0cf33c
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-18 16:42:23 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int vowels,constant;
+printf("enter the characters");
+scanf("%d",&characters);
+if(character=a)
+{
+printf("vowel");
+}
+else
+{
+printf("constant");
+}
+}

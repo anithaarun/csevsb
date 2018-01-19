2e4aa16223d5f4cc65e0b65862d88b8b758de160
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-19 13:47:01 +0530

    Create primes.c

---
+++ primes.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("enter the number between two intrevels");
+}
+else
+{
+printf("not print");
+}
+}

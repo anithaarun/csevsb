2c3e7b05305b004bedde06971edf5c5587a754a5
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-21 09:46:48 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+int first,diff,terms,value,sum=0,i;
+printf("enter the number of terms in ap series\n");
+scanf("%d",&terms);
+printf("enter the first and common difference of ap series\n");
+scanf("%d%d",&first term,&diff);
+value=first;
+printf("AP series\n");
+for(i=0;i<terms;i++)
+printf("%d",value);
+value=value+diff;
+{
+printf("sum of Ap series till %d terms is %d\n");
+getch();
+return 0;
+}
+}

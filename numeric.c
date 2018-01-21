03a680f280b98d6330cda298657955d0c66f6f9b
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-21 11:18:27 +0530

    Create numeric.c

---
+++ numeric.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int value;
+printf("enter the value");
+scanf("%d",&value);
+if(value=0;value<9;value++)
+{
+printf("numeric");
+}
+else
+{
+printf("not numeric");
+}
+}

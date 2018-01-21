3c506f27846399da8dc6815189b2545c6c04a2a7
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-21 10:20:04 +0530

    Create arrayy.c

---
+++ arrayy.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i,a[100],n;
+{
+printf("enter the number of elements to store in the array");
+scanf("%d",&n);
+ printf("number of elemente in the array\n);
+ for(i=0;i<n;i++)
+ {
+ printf("element-%d",&a[i]);
+ }
+ printf("the values stored in the array are \n");
+ for(i=0;i<n;i++)
+ {
+ printf("%5d",&a[i]);
+ }
+ }

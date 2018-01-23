9bb61e1b28cef71e65779a0c2f7547b1c096493c
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-23 16:26:54 +0530

    Update swapm.c

--- swapm.c
+++ swapm.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-int main() {
+#include<conio.h>
+int main() 
+{
     int i = 65;
-    int k = 120;
+    int k = 12;
     printf(" value of i=%d k=%d before swapping", i, k);
-
-    i = i ^ k;
+    i = i ^ k; 
     k = i ^ k;
-    i = i ^ k;
     printf("value of i=%d k=%d after swapping", i, k);
-
     return 0;
 }

c664b98406e624ff637c366c9b670e444fbaded2
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-25 16:16:39 +0530

    Create concat.c

---
+++ concat.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char s1[20],s2[10];
+printf("enter the first string");
+scanf("%d",&s1);
+gets(s1);
+printf("enter the second string");
+scanf("%d",&s2);
+gets(s2);
+if(s1[20]>s2[10])
+{
+printf("enter the value of s1");
+}
+elseif
+{
+printf("enter the values of s2");
+}
+else
+{
+printf("enter the values of s1&s2");
+}
+}

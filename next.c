537cde2dc05030244a66db1186b56d4c9735486c
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-01-25 16:37:26 +0530

    Create next.c

---
+++ next.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+  n++;
+  printf("enter the next number");
+  scanf("%d",&n++);
+  {
+  printf("the next number is %d n");
+  }
+  }

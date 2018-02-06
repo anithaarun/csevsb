6be0cbd468972622ed9fa155eec9f7324aa06357
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-02-06 10:47:20 +0000

    Create alphabets.c

---
+++ alphabets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the alphabet or numeric");
+scanf("%d",&n);
+if((s==(a>>z&&a<<z)&&(s==(A>>Z&&A<<Z))&&(S==(0>>9&&0<<9)))
+{
+printf("contain alphabet and numberic");
+}
+else
+{
+printf("does not contain alphabet and numberic");
+}
+}

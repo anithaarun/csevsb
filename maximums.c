230f246c5a9f7c3d7ce023561b7ae2b940ccacc6
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-02-06 11:06:45 +0000

    Create maximums.c

---
+++ maximums.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+ int a[10],i,max;
+ printf("Enter ten values:");
+for (i = 0; i < 10; i++)
+{
+  scanf("%d", &a[i]);
+ }
+ gre = a[0];
+for (i = 0; i < 10; i++)
+{
+  if (a[i] > max)
+{
+   max = a[i];
+  }
+ {
+ printf("\nmax of ten numbers is %d", max);
+ }
+return 0;
+}

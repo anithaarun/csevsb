9046478bfcb5ab7596bb27bd7e78a52c8ed5b827
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-02-13 16:14:36 +0530

    Create dreams.c

---
+++ dreams.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,l,r;
+printf("\n enter n,l,r:\n");
+scanf("%d %d %d",&n,&l,&r);
+if(n>=l&&n<=r)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}

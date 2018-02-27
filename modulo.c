026f689b3b20aea06ecd5b42c775ca0b17922d5a
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-02-27 16:36:45 +0530

    Create modulo.c

---
+++ modulo.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main () 
+{
+int a, b, mod, division;
+printf ("enter the value of a:");
+scanf ("%d", &a);
+printf ("enter the value of b:");
+scanf ("%d", &b);
+mod = a % b;
+division = a/b;
+printf ("%d\n",mod);
+printf("%d\n",division);
+return 0;
+}
+
+
+ 
+ 

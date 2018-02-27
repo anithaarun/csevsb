021d9425c260ba6d1039060bdb5c5b2926853e3d
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-02-27 16:15:17 +0530

    Create farm.c

---
+++ farm.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float l,b,area;
+printf("\nenter the length of the farm:");
+scanf("%f",&l);
+printf("\nenter the breadth of the farm:");
+scanf("%f",&b);
+area=l*b;
+printf("\nthe area is %f",area);
+return 0;
+}

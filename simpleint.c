c899a57047862c1a2be52b0d948e2111bf252cee
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-03-08 16:29:41 +0530

    Create simpleint.c

---
+++ simpleint.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float P,T,R,si;
+    scanf("%f%f%f",&P,&T,&R);
+    si=(P*T*R)/100;
+    {
+    printf("\n the simple intrest is:%d",si);
+    }
+    return 0;
+}

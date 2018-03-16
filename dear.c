51ce377e89c261a1b85a01887faf4665424caa61
anithaarun <31502990+anithaarun@users.noreply.github.com>
2018-03-16 14:56:32 +0530

    Create dear.c

---
+++ dear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,C,D;
+    printf("enter the value of A:");
+    scanf("%d",&A);
+    printf("\nenter the value of B:");
+    scanf("%d",&B);
+    printf("\nenter the value of C:");
+    scanf("%d",&C);
+    D=(A*B)/C;
+    {
+        printf("\n enter the value of D is: %d",D);
+    }
+    return 0;
+}
